af6a7480bd108fd5be7f8ca45d1adbca96dd89ed
roxifries <pineda.aynnarose@auf.edu.ph>
2022-04-11 13:54:39 +0800

    added chapters to all chapter blade

--- resources/views/matthew/all.blade.php
+++ resources/views/matthew/all.blade.php
@@ -1,5 +1,6 @@
 <h1>{{ $bible_version }}</h1>
 <h2>{{ $book_name }}</h2>
+
 <h3>Chapter 1</h3>
 <pre>
 @include('matthew.chapter1')
@@ -19,3 +20,134 @@
 <pre>
 @include('matthew.chapter4')
 </pre>
+
+<h3>Chapter 5</h3>
+<pre>
+@include('matthew.chapter5')
+</pre>
+
+<h3>Chapter 6</h3>
+<pre>
+@include('matthew.chapter6')
+</pre>
+
+<h3>Chapter 7</h3>
+<pre>
+@include('matthew.chapter7')
+</pre>
+
+<h3>Chapter 8</h3>
+<pre>
+@include('matthew.chapter8')
+</pre>
+
+<h3>Chapter 9</h3>
+<pre>
+@include('matthew.chapter9')
+</pre>
+
+<h3>Chapter 10</h3>
+<pre>
+@include('matthew.chapter10')
+</pre>
+
+<h3>Chapter 11</h3>
+<pre>
+@include('matthew.chapter11')
+</pre>
+
+<h3>Chapter 12</h3>
+<pre>
+@include('matthew.chapter12')
+</pre>
+
+<h3>Chapter 13</h3>
+<pre>
+@include('matthew.chapter13')
+</pre>
+
+<h3>Chapter 14</h3>
+<pre>
+@include('matthew.chapter14')
+</pre>
+
+<h3>Chapter 15</h3>
+<pre>
+@include('matthew.chapter15')
+</pre>
+
+<h3>Chapter 16</h3>
+<pre>
+@include('matthew.chapter16')
+</pre>
+
+<h3>Chapter 17</h3>
+<pre>
+@include('matthew.chapter17')
+</pre>
+
+<h3>Chapter 18</h3>
+<pre>
+@include('matthew.chapter18')
+</pre>
+
+<h3>Chapter 19</h3>
+<pre>
+@include('matthew.chapter19')
+</pre>
+
+<h3>Chapter 20</h3>
+<pre>
+@include('matthew.chapter20')
+</pre>
+
+<h3>Chapter 21</h3>
+<pre>
+@include('matthew.chapter21')
+</pre>
+
+<h3>Chapter 22</h3>
+<pre>
+@include('matthew.chapter22')
+</pre>
+
+<h3>Chapter 23</h3>
+<pre>
+@include('matthew.chapter23')
+</pre>
+
+
+<h3>Chapter 24</h3>
+<pre>
+@include('matthew.chapter24')
+</pre>
+
+<h3>Chapter 25</h3>
+<pre>
+@include('matthew.chapter25')
+</pre>
+
+<h3>Chapter 26</h3>
+<pre>
+@include('matthew.chapter26')
+</pre>
+
+<h3>Chapter 27</h3>
+<pre>
+@include('matthew.chapter27')
+</pre>
+
+<h3>Chapter 28</h3>
+<pre>
+@include('matthew.chapter28')
+</pre>
+ 
+
+
+
+
+
+
+
+
+
